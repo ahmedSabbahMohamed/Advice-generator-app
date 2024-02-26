9f56c97f158953f900b4810f9171b8c4b9ffc6d0
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-02-26 20:54:43 +0200

    improved code

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 let id = document.getElementById("advice-id"),
-adviceTxt = document.getElementById("advice"),
-btn = document.querySelector("button");
-
+adviceTxt = document.getElementById("advice-text");
 async function getAdvice () {
-    await fetch("https://api.adviceslip.com/advice").then(result => {
-        let data = result.json();
-        return data;
-    }).then(data => {
-        id.innerHTML = data.slip.id;
-        adviceTxt.innerHTML = data.slip.advice;
-    });
+    adviceTxt.innerHTML = "Loading..."
+        try {
+          await fetch("https://api.adviceslip.com/advice")
+            .then((result) => {
+              let data = result.json();
+              return data;
+            })
+            .then((data) => {
+              id.innerHTML = data.slip.id;
+              adviceTxt.innerHTML = data.slip.advice;
+            });
+        } catch (err) {
+          console.log("Error fething advice");
+          adviceTxt.innerHTML = "Error loading advice";
+        }
 }
 
 getAdvice();
-
-btn.addEventListener("click", _ => {
-    getAdvice();
-});
